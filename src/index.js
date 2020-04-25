35f0a35288a8a49a6bc537cd484994647471baaa
Ilona <hair.masterpiece@gmail.com>
2020-04-25 18:06:13 +0300

    Сделала домашнее задане

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const fio = window.prompt(' Ваше фамилия,имя,отчесво?');
+const age = parseInt(window.prompt(' Ваш возраст в годах?'));
+ const ageInDays = 365 * age;
+ const for5Year = age + 5;
+const sex = window.confirm(' Ваш пол-мужской?');
+const penciaAge = 65;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let pp ;
+ if (sex) {
+    pp ='мужской';
+ } else {
+     pp ='женский';
+ }
+
+let tt ;
+
+ if (penciaAge > age) {
+     tt = 'нет';
+} else {
+     tt = 'да';
+ }
+
+let result = "ваше ФИО:"+ fio +"\nваш возраст в годах:"+ age +"\nваш возраст в днях:"+ ageInDays +"\nчерез 5 лет вам будет: "+ for5Year+"\nваш пол: " + pp +"\nвы на пенсии: " + tt;
+
+window.alert(tt);
+ alert(result);
+
